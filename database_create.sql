a58908294cf9b4390c566a8ff47ad25a6cc9d89b
fedrahc <220705945+fedrahc@users.noreply.github.com>
2026-05-02 16:03:12 +0200

    Update database_create.sql

--- database_create.sql
+++ database_create.sql
@@ -1,267 +1,132 @@
--- MySQL Workbench Forward Engineering
+DROP SCHEMA IF EXISTS mydb;
+CREATE SCHEMA mydb DEFAULT CHARACTER SET utf8;
+USE mydb;
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+create table PERSONNE (
+    id_personne      int         not null,
+    nom              varchar(45) not null,
+    prenom           varchar(45) not null,
+    date_naissance   date        not null,
+    adresse          varchar(45),
+    telephone        varchar(45),
+    email            varchar(45),
+    primary key (id_personne));
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-USE modelV2
--- -----------------------------------------------------
--- Table  PERSONNE 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS PERSONNE  (
-   id_personne  INT NOT NULL,
-   nom  VARCHAR(45) NOT NULL,
-   prenom  VARCHAR(45) NOT NULL,
-   date_naissance  DATE NOT NULL,
-   adresse  VARCHAR(45) NULL,
-   téléphone  VARCHAR(45) NULL,
-   email  VARCHAR(45) NULL,
-  PRIMARY KEY ( id_personne ))
+create table ENTREPRISE (
+    id_entreprise  int         not null,
+    nom            varchar(45) not null,
+    primary key (id_entreprise));
 
+create table COMPETENCE (
+    id_competence  int         not null,
+    libelle        varchar(45) not null,
+    primary key (id_competence));
 
+create table METIER (
+    id_metier  int         not null,
+    libelle    varchar(45) not null,
+    primary key (id_metier));
 
--- -----------------------------------------------------
--- Table  ENTREPRISE 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS ENTREPRISE (
-   id_entreprise  INT NOT NULL,
-   nom  VARCHAR(45) NOT NULL,
-  PRIMARY KEY ( id_entreprise ))
+create table SALARIE (
+    id_salarie           int         not null,
+    `role`               varchar(45) not null,
+    PERSONNE_id_personne int         not null,
+    primary key (id_salarie, PERSONNE_id_personne),
+    foreign key (PERSONNE_id_personne) references PERSONNE(id_personne)
+        on delete cascade on update cascade);
 
+create table CANDIDAT (
+    id_candidat          int         not null,
+    description          varchar(45),
+    disponibilite        varchar(45) not null,
+    PERSONNE_id_personne int         not null,
+    primary key (id_candidat),
+    foreign key (PERSONNE_id_personne) references PERSONNE(id_personne)
+        on delete cascade on update cascade);
 
+create table OFFRE_EMPLOI (
+    id_offre                 int         not null,
+    intitule                 varchar(45) not null,
+    taux_horaire             decimal     not null,
+    heures_mensuel           int         not null,
+    date_debut               date        not null,
+    date_fin                 date,
+    statut                   varchar(45) not null,
+    type_contrat             varchar(45) not null,
+    date_fermeture           date,
+    ENTREPRISE_id_entreprise int         not null,
+    SALARIE_id_salarie       int,
+    primary key (id_offre, ENTREPRISE_id_entreprise),
+    foreign key (ENTREPRISE_id_entreprise) references ENTREPRISE(id_entreprise)
+        on delete cascade on update cascade,
+    foreign key (SALARIE_id_salarie) references SALARIE(id_salarie)
+        on delete no action on update no action);
 
--- -----------------------------------------------------
--- Table  COMPETENCE 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS  COMPETENCE  (
-   id_competence  INT NOT NULL,
-   libelle  VARCHAR(45) NOT NULL,
-  PRIMARY KEY ( id_competence ))
+create table DIPLOME (
+    id_diplome           int         not null,
+    libelle              varchar(45) not null,
+    niveau_etudes        varchar(45) not null,
+    CANDIDAT_id_candidat int         not null,
+    primary key (id_diplome, CANDIDAT_id_candidat),
+    foreign key (CANDIDAT_id_candidat) references CANDIDAT(id_candidat)
+        on delete cascade on update cascade);
 
+create table EXPERIENCE_PRO (
+    id_experience        int         not null,
+    type_contrat         varchar(45) not null,
+    date_debut           date        not null,
+    date_fin             date,
+    CANDIDAT_id_candidat int         not null,
+    primary key (id_experience, CANDIDAT_id_candidat),
+    foreign key (CANDIDAT_id_candidat) references CANDIDAT(id_candidat)
+        on delete cascade on update cascade);
 
+create table CANDIDATURE (
+    statut_candidature    int not null,
+    CANDIDAT_id_candidat  int not null,
+    OFFRE_EMPLOI_id_offre int not null,
+    primary key (CANDIDAT_id_candidat, OFFRE_EMPLOI_id_offre),
+    foreign key (CANDIDAT_id_candidat) references CANDIDAT(id_candidat)
+        on delete cascade on update cascade,
+    foreign key (OFFRE_EMPLOI_id_offre) references OFFRE_EMPLOI(id_offre)
+        on delete cascade on update cascade);
 
--- -----------------------------------------------------
--- Table  METIER 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS  METIER  (
-   id_metier  INT NOT NULL,
-   libelle  VARCHAR(45) NOT NULL,
-  PRIMARY KEY ( id_metier ))
+create table CANDIDAT_METIER (
+    CANDIDAT_id_candidat int not null,
+    METIER_id_metier     int not null,
+    primary key (CANDIDAT_id_candidat, METIER_id_metier),
+    foreign key (CANDIDAT_id_candidat) references CANDIDAT(id_candidat)
+        on delete cascade on update cascade,
+    foreign key (METIER_id_metier) references METIER(id_metier)
+        on delete no action on update no action);
 
+create table CANDIDAT_has_COMPETENCE (
+    CANDIDAT_id_candidat     int not null,
+    COMPETENCE_id_competence int not null,
+    primary key (CANDIDAT_id_candidat, COMPETENCE_id_competence),
+    foreign key (CANDIDAT_id_candidat) references CANDIDAT(id_candidat)
+        on delete cascade on update cascade,
+    foreign key (COMPETENCE_id_competence) references COMPETENCE(id_competence)
+        on delete no action on update no action);
 
+create table OFFRE_EMPLOI_has_COMPETENCE (
+    OFFRE_EMPLOI_id_offre                 int not null,
+    OFFRE_EMPLOI_ENTREPRISE_id_entreprise int not null,
+    COMPETENCE_id_competence              int not null,
+    primary key (OFFRE_EMPLOI_id_offre, OFFRE_EMPLOI_ENTREPRISE_id_entreprise, COMPETENCE_id_competence),
+    foreign key (OFFRE_EMPLOI_id_offre, OFFRE_EMPLOI_ENTREPRISE_id_entreprise)
+        references OFFRE_EMPLOI(id_offre, ENTREPRISE_id_entreprise)
+        on delete cascade on update cascade,
+    foreign key (COMPETENCE_id_competence) references COMPETENCE(id_competence)
+        on delete no action on update no action);
 
--- -----------------------------------------------------
--- Table  SALARIE 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS  SALARIE  (
-   id_salarie  INT NOT NULL,
-   role  VARCHAR(45) NOT NULL,
-   PERSONNE_id_personne  INT NOT NULL,
-  PRIMARY KEY ( id_salarie ,  PERSONNE_id_personne ),
-  INDEX  fk_SALARIE_PERSONNE1_idx  ( PERSONNE_id_personne  ASC) VISIBLE,
-  CONSTRAINT  fk_SALARIE_PERSONNE1 
-    FOREIGN KEY ( PERSONNE_id_personne )
-    REFERENCES  PERSONNE  ( id_personne )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-
-
-
--- -----------------------------------------------------
--- Table  CANDIDAT 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS  CANDIDAT  (
-   id_candidat  INT NOT NULL,
-   description  VARCHAR(45) NULL,
-   disponibilite  VARCHAR(45) NOT NULL,
-   PERSONNE_id_personne  INT NOT NULL,
-  PRIMARY KEY ( id_candidat ,  PERSONNE_id_personne ),
-  INDEX  fk_CANDIDAT_PERSONNE1_idx  ( PERSONNE_id_personne  ASC) VISIBLE,
-  CONSTRAINT  fk_CANDIDAT_PERSONNE1 
-    FOREIGN KEY ( PERSONNE_id_personne )
-    REFERENCES  PERSONNE  ( id_personne )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-
-
-
--- -----------------------------------------------------
--- Table  OFFRE_EMPLOI 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS  OFFRE_EMPLOI  (
-   id_offre  INT NOT NULL,
-   intitule  VARCHAR(45) NOT NULL,
-   taux_horaire  DECIMAL NOT NULL,
-   heures_mensuel  INT NOT NULL,
-   date_debut  DATE NOT NULL,
-   date_fin  DATE NULL,
-   statut  VARCHAR(45) NOT NULL,
-   type_contrat  VARCHAR(45) NOT NULL,
-   date_fermeture  DATE NULL,
-   ENTREPRISE_id_entreprise  INT NOT NULL,
-   SALARIE_id_salarie  INT NULL,
-  PRIMARY KEY ( id_offre ,  ENTREPRISE_id_entreprise ),
-  INDEX  fk_OFFRE_EMPLOI_ENTREPRISE_idx  ( ENTREPRISE_id_entreprise  ASC) VISIBLE,
-  INDEX  fk_OFFRE_EMPLOI_SALARIE1_idx  ( SALARIE_id_salarie  ASC) VISIBLE,
-  CONSTRAINT  fk_OFFRE_EMPLOI_ENTREPRISE 
-    FOREIGN KEY ( ENTREPRISE_id_entreprise )
-    REFERENCES  ENTREPRISE  ( id_entreprise )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT  fk_OFFRE_EMPLOI_SALARIE1 
-    FOREIGN KEY ( SALARIE_id_salarie )
-    REFERENCES  SALARIE  ( id_salarie )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-
-
-
--- -----------------------------------------------------
--- Table  DIPLOME 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS  DIPLOME  (
-   id_diplome  INT NOT NULL,
-   libelle  VARCHAR(45) NOT NULL,
-   niveau_etudes  VARCHAR(45) NOT NULL,
-   CANDIDAT_id_candidat  INT NOT NULL,
-  PRIMARY KEY ( id_diplome ,  CANDIDAT_id_candidat ),
-  INDEX  fk_DIPLOME_CANDIDAT1_idx  ( CANDIDAT_id_candidat  ASC) VISIBLE,
-  CONSTRAINT  fk_DIPLOME_CANDIDAT1 
-    FOREIGN KEY ( CANDIDAT_id_candidat )
-    REFERENCES  CANDIDAT  ( id_candidat )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-
-
-
--- -----------------------------------------------------
--- Table  EXPERIENCE_PRO 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS  EXPERIENCE_PRO  (
-   id_experience  INT NOT NULL,
-   type_contrat  VARCHAR(45) NOT NULL,
-   date_debut  DATE NOT NULL,
-   date_fin  DATE NULL,
-   CANDIDAT_id_candidat  INT NOT NULL,
-  PRIMARY KEY ( id_experience ,  CANDIDAT_id_candidat ),
-  INDEX  fk_EXPERIENCE_PRO_CANDIDAT1_idx  ( CANDIDAT_id_candidat  ASC) VISIBLE,
-  CONSTRAINT  fk_EXPERIENCE_PRO_CANDIDAT1 
-    FOREIGN KEY ( CANDIDAT_id_candidat )
-    REFERENCES  CANDIDAT  ( id_candidat )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-
-
-
--- -----------------------------------------------------
--- Table  CANDIDATURE 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS  CANDIDATURE  (
-   statut_candidature  INT NOT NULL,
-   CANDIDAT_id_candidat  INT NOT NULL,
-   OFFRE_EMPLOI_id_offre  INT NOT NULL,
-  PRIMARY KEY ( CANDIDAT_id_candidat ,  OFFRE_EMPLOI_id_offre ),
-  INDEX  fk_CANDIDATURE_OFFRE_EMPLOI1_idx  ( OFFRE_EMPLOI_id_offre  ASC) VISIBLE,
-  CONSTRAINT  fk_CANDIDATURE_CANDIDAT1 
-    FOREIGN KEY ( CANDIDAT_id_candidat )
-    REFERENCES  CANDIDAT  ( id_candidat )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT  fk_CANDIDATURE_OFFRE_EMPLOI1 
-    FOREIGN KEY ( OFFRE_EMPLOI_id_offre )
-    REFERENCES  OFFRE_EMPLOI  ( id_offre )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-
-
-
--- -----------------------------------------------------
--- Table  CANDIDAT_METIER 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS  CANDIDAT_METIER  (
-   CANDIDAT_id_candidat  INT NOT NULL,
-   METIER_id_metier  INT NOT NULL,
-  PRIMARY KEY ( CANDIDAT_id_candidat ,  METIER_id_metier ),
-  INDEX  fk_CANDIDAT_METIER_METIER1_idx  ( METIER_id_metier  ASC) VISIBLE,
-  CONSTRAINT  fk_CANDIDAT_METIER_CANDIDAT1 
-    FOREIGN KEY ( CANDIDAT_id_candidat )
-    REFERENCES  CANDIDAT  ( id_candidat )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT  fk_CANDIDAT_METIER_METIER1 
-    FOREIGN KEY ( METIER_id_metier )
-    REFERENCES  METIER  ( id_metier )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-
-
-
--- -----------------------------------------------------
--- Table  CANDIDAT_has_COMPETENCE 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS  CANDIDAT_has_COMPETENCE  (
-   CANDIDAT_id_candidat  INT NOT NULL,
-   COMPETENCE_id_competence  INT NOT NULL,
-  PRIMARY KEY ( CANDIDAT_id_candidat ,  COMPETENCE_id_competence ),
-  INDEX  fk_CANDIDAT_has_COMPETENCE_COMPETENCE1_idx  ( COMPETENCE_id_competence  ASC) VISIBLE,
-  INDEX  fk_CANDIDAT_has_COMPETENCE_CANDIDAT1_idx  ( CANDIDAT_id_candidat  ASC) VISIBLE,
-  CONSTRAINT  fk_CANDIDAT_has_COMPETENCE_CANDIDAT1 
-    FOREIGN KEY ( CANDIDAT_id_candidat )
-    REFERENCES  CANDIDAT  ( id_candidat )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT  fk_CANDIDAT_has_COMPETENCE_COMPETENCE1 
-    FOREIGN KEY ( COMPETENCE_id_competence )
-    REFERENCES  COMPETENCE  ( id_competence )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-
-
-
--- -----------------------------------------------------
--- Table  OFFRE_EMPLOI_has_COMPETENCE 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS  OFFRE_EMPLOI_has_COMPETENCE  (
-   OFFRE_EMPLOI_id_offre  INT NOT NULL,
-   OFFRE_EMPLOI_ENTREPRISE_id_entreprise  INT NOT NULL,
-   COMPETENCE_id_competence  INT NOT NULL,
-  PRIMARY KEY ( OFFRE_EMPLOI_id_offre ,  OFFRE_EMPLOI_ENTREPRISE_id_entreprise ,  COMPETENCE_id_competence ),
-  INDEX  fk_OFFRE_EMPLOI_has_COMPETENCE_COMPETENCE1_idx  ( COMPETENCE_id_competence  ASC) VISIBLE,
-  INDEX  fk_OFFRE_EMPLOI_has_COMPETENCE_OFFRE_EMPLOI1_idx  ( OFFRE_EMPLOI_id_offre  ASC,  OFFRE_EMPLOI_ENTREPRISE_id_entreprise  ASC) VISIBLE,
-  CONSTRAINT  fk_OFFRE_EMPLOI_has_COMPETENCE_OFFRE_EMPLOI1 
-    FOREIGN KEY ( OFFRE_EMPLOI_id_offre  ,  OFFRE_EMPLOI_ENTREPRISE_id_entreprise )
-    REFERENCES  OFFRE_EMPLOI  ( id_offre  ,  ENTREPRISE_id_entreprise )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT  fk_OFFRE_EMPLOI_has_COMPETENCE_COMPETENCE1 
-    FOREIGN KEY ( COMPETENCE_id_competence )
-    REFERENCES  COMPETENCE  ( id_competence )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-
-
-
--- -----------------------------------------------------
--- Table  OFFRE_EMPLOI_has_METIER 
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS  OFFRE_EMPLOI_has_METIER  (
-   OFFRE_EMPLOI_id_offre  INT NOT NULL,
-   OFFRE_EMPLOI_ENTREPRISE_id_entreprise  INT NOT NULL,
-   METIER_id_metier  INT NOT NULL,
-  PRIMARY KEY ( OFFRE_EMPLOI_id_offre ,  OFFRE_EMPLOI_ENTREPRISE_id_entreprise ,  METIER_id_metier ),
-  INDEX  fk_OFFRE_EMPLOI_has_METIER_METIER1_idx  ( METIER_id_metier  ASC) VISIBLE,
-  INDEX  fk_OFFRE_EMPLOI_has_METIER_OFFRE_EMPLOI1_idx  ( OFFRE_EMPLOI_id_offre  ASC,  OFFRE_EMPLOI_ENTREPRISE_id_entreprise  ASC) VISIBLE,
-  CONSTRAINT  fk_OFFRE_EMPLOI_has_METIER_OFFRE_EMPLOI1 
-    FOREIGN KEY ( OFFRE_EMPLOI_id_offre  ,  OFFRE_EMPLOI_ENTREPRISE_id_entreprise )
-    REFERENCES  OFFRE_EMPLOI  ( id_offre  ,  ENTREPRISE_id_entreprise )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT  fk_OFFRE_EMPLOI_has_METIER_METIER1 
-    FOREIGN KEY ( METIER_id_metier )
-    REFERENCES METIER ( id_metier )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-
+create table OFFRE_EMPLOI_has_METIER (
+    OFFRE_EMPLOI_id_offre                 int not null,
+    OFFRE_EMPLOI_ENTREPRISE_id_entreprise int not null,
+    METIER_id_metier                      int not null,
+    primary key (OFFRE_EMPLOI_id_offre, OFFRE_EMPLOI_ENTREPRISE_id_entreprise, METIER_id_metier),
+    foreign key (OFFRE_EMPLOI_id_offre, OFFRE_EMPLOI_ENTREPRISE_id_entreprise)
+        references OFFRE_EMPLOI(id_offre, ENTREPRISE_id_entreprise)
+        on delete cascade on update cascade,
+    foreign key (METIER_id_metier) references METIER(id_metier)
+        on delete no action on update no action);
